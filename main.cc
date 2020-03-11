7957a7e7ad76fa6883c54fdfb96895f276f08add
Jerry Leung <jerrytfleung@users.noreply.github.com>
2020-03-11 15:07:06 -0700

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <initializer_list>
+using namespace std;
+
+template<typename T>
+class object {
+ public:
+  object() { cout << "Default ctor!" << endl; }
+  object(T size, T default_value) { cout << "Parameterized ctor!" << endl; }
+  object(initializer_list<T> init) { cout << "Initializer list ctor!" << endl; }
+  void print() { cout << "print() is called!" << endl; } 
+};
+
+int main() {
+  object<int> o1;
+  o1.print();
+  object<int> o2(10, 1);
+  o2.print();
+  object<int> o3{10, 1};
+  o3.print();
+  object<int> o4{};
+  o4.print();
+  object<int> o5();
+  // o5.print(); compile error
+  /**
+   * error: request for member ‘print’ in ‘o5’, which is of non-class type ‘object<int>()’
+   * o5.print();
+   *    ^~~~~
+   */
+  return 0;
+}
